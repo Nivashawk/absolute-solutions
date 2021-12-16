44e64a97682b5623d78b8d1a98f4563874ce3ba4
Nivashawk <46569236+Nivashawk@users.noreply.github.com>
2021-12-17 02:46:01 +0530

    small fix

--- src/controller/admin.controller.js
+++ src/controller/admin.controller.js
@@ -1,8 +1,8 @@
 const Adminmodel = require("../model/admin.model");
-const base_controller = require("./base.controller");
-const _response = require("../response/response");
+const BaseController = require("./base.controller");
+const Response = require("../response/response");
 const Message_response = require("../response/messages");
-const query = require("../model/query");
+const Query = require("../model/query");
 
 // ### create Admin document ###
 
@@ -24,26 +24,26 @@ const create = async (req, res) => {
       total_number_of_documents === 0
     ) {
       await admin.save();
-      const response = _response.success(Message_response.Insert);
+      const response = Response.success(Message_response.Insert);
       return res.status(200).json(response);
     } else {
       const find_document_with_user_id = await Adminmodel.find(
-        query.find_user(req.body.User_id)
+        Query.find_user(req.body.User_id)
       );
       //   console.log(find_document_with_user_id.length);
       if (find_document_with_user_id.length !== 0) {
-        const response = _response.error(
+        const response = Response.error(
           Message_response.Already_exits("user_id", req.body.User_id)
         );
         res.status(200).json(response);
       } else if (find_document_with_user_id.length === 0) {
         await admin.save();
-        const response = _response.success(Message_response.Insert);
+        const response = Response.success(Message_response.Insert);
         return res.status(200).json(response);
       }
     }
   };
-  base_controller.base(_base);
+  BaseController.base(_base);
 };
 
 // ### Admin Login ###
@@ -56,31 +56,31 @@ const login = async (req, res) => {
       total_number_of_documents === undefined ||
       total_number_of_documents === 0
     ) {
-      const response = _response.error(Message_response.Empty_database);
+      const response = Response.error(Message_response.Empty_database);
       res.status(200).json(response);
     } else {
       const login_with_username_and_password = await Adminmodel.find(
-        query.login(req.body.Username, req.body.Password)
+        Query.login(req.body.Username, req.body.Password)
       );
       //   console.log("login_results",login_with_username_and_password[0].User_id);
       if (login_with_username_and_password.length !== 0) {
         const result = await Adminmodel.updateOne(
-          query.update_notification_token(
+          Query.update_notification_token(
             login_with_username_and_password[0].User_id,
             req.body.Notification_Token
           )
         );
         if (result) {
-          const response = _response.success(Message_response.Login);
+          const response = Response.success(Message_response.Login);
           return res.status(200).json(response);
         } else {
-          const response = _response.error(
+          const response = Response.error(
             Message_response.Not_updated("notification token")
           );
           return res.status(200).json(response);
         }
       } else {
-        const response = _response.error(
+        const response = Response.error(
           Message_response.Invalid_credentials(
             req.body.Username,
             req.body.Password
@@ -90,7 +90,7 @@ const login = async (req, res) => {
       }
     }
   };
-  base_controller.base(_base);
+  BaseController.base(_base);
 };
 
 module.exports = {
